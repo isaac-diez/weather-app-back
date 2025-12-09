8789011f3389bbd03754e8ae679ab337c219dc00
isaac diez <isaac.diez@proton.me>
2025-12-09 17:40:27 +0100

    Feat(Client): created WeatherApiClient

---
+++ src/main/java/com/isaac/weatherapp/client/WeatherApiClient.java
@@ -0,0 +1,36 @@
+package com.isaac.weatherapp.client;
+
+import com.isaac.weatherapp.dto.WeatherResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+
+@Component
+@Slf4j
+public class WeatherApiClient {
+
+    private final WebClient weatherWebClient;
+
+    public WeatherApiClient(@Qualifier("weatherWebClient") WebClient weatherWebClient) {
+        this.weatherWebClient = weatherWebClient;
+    }
+
+    public WeatherResponse getWeatherData(double latitude, double longitude) {
+
+            return weatherWebClient.get()
+                    .uri(uriBuilder -> uriBuilder
+                            .path("/v1/forecast")
+                            .queryParam("latitude", latitude)
+                            .queryParam("longitude", longitude)
+                            .queryParam("current", "temperature_2m,relative_humidity_2m,apparent_temperature,is_day,precipitation,cloud_cover")
+                            .queryParam("daily", "apparent_temperature_min,precipitation_probability_max,apparent_temperature_max")
+                            .queryParam("hourly", "temperature_2m,relative_humidity_2m,apparent_temperature,precipitation_probability")
+                            .queryParam("timezone", "Europe/Berlin")
+                            .build())
+                    .retrieve()
+                    .bodyToMono(WeatherResponse.class)
+                    .block();
+        }
+}
