f9d0cb20d762fefe447795c88152a007da05b8d1
isaac diez <isaac.diez@proton.me>
2025-12-09 17:39:47 +0100

    Feat(Client): created GeocodingApiClient

---
+++ src/main/java/com/isaac/weatherapp/client/GeocodingApiClient.java
@@ -0,0 +1,34 @@
+package com.isaac.weatherapp.client;
+
+import com.isaac.weatherapp.dto.CityResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Component
+@Slf4j
+public class GeocodingApiClient {
+
+    private final WebClient geocodingWebClient;
+
+    public GeocodingApiClient(@Qualifier("geocodingWebClient") WebClient geocodingWebClient) {
+        this.geocodingWebClient = geocodingWebClient;
+    }
+
+    public CityResponse getCityData(String city) {
+        log.info("Calling GeoCoding API for city: {}", city);
+
+        return geocodingWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/v1/search")
+                        .queryParam("name", city)
+                        .build())
+                .retrieve()
+                .bodyToMono(CityResponse.class)
+                .block();
+    }
+}
