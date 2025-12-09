aebf12cdb3659e2e5b8daa84c115b3a41c831b6b
isaac diez <isaac.diez@proton.me>
2025-12-09 17:39:28 +0100

    Feat(Client): created WebClientConfig

---
+++ src/main/java/com/isaac/weatherapp/config/WebClientConfig.java
@@ -0,0 +1,27 @@
+package com.isaac.weatherapp.config;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Configuration
+public class WebClientConfig {
+
+    @Bean
+    @Qualifier("weatherWebClient")
+    public WebClient weatherWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("https://api.open-meteo.com")
+                .build();
+    }
+
+    @Bean
+    @Qualifier("geocodingWebClient")
+    public WebClient geocodingWebClient(WebClient.Builder builder) {
+        return builder
+                .baseUrl("https://geocoding-api.open-meteo.com")
+                .build();
+    }
+
+}
