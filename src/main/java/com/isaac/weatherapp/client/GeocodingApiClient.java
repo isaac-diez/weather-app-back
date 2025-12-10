72e090a0f0c23182f4e6a053b1a4478bcab8a330
isaac diez <isaac.diez@proton.me>
2025-12-10 18:16:10 +0100

    Refactor(APIclient): added count=20 cities in list

--- src/main/java/com/isaac/weatherapp/client/GeocodingApiClient.java
+++ src/main/java/com/isaac/weatherapp/client/GeocodingApiClient.java
@@ -5,9 +5,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
-import org.springframework.web.util.UriComponentsBuilder;
-
-import java.net.URI;
 
 @Component
 @Slf4j
@@ -26,6 +23,7 @@ public class GeocodingApiClient {
                 .uri(uriBuilder -> uriBuilder
                         .path("/v1/search")
                         .queryParam("name", city)
+                        .queryParam("count", 20)
                         .build())
                 .retrieve()
                 .bodyToMono(CityResponse.class)
