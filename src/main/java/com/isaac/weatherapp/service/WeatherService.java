744d36c54090508cd34101840b3bc149b63bcb53
isaac diez <isaac.diez@proton.me>
2026-03-18 19:15:43 +0100

    refactor: combined getCurrentWeather() and getForecast() to the new getFullWeather() method

--- src/main/java/com/isaac/weatherapp/service/WeatherService.java
+++ src/main/java/com/isaac/weatherapp/service/WeatherService.java
@@ -1,11 +1,9 @@
 package com.isaac.weatherapp.service;
 
 import com.isaac.weatherapp.dto.CityDTO;
-import com.isaac.weatherapp.dto.CurrentWeatherDTO;
-import com.isaac.weatherapp.dto.ForecastDTO;
+import com.isaac.weatherapp.dto.FullWeatherDTO;
 
 public interface WeatherService {
 
-    CurrentWeatherDTO getCurrentWeather(CityDTO city);
-    ForecastDTO getForecast(CityDTO city);
+    FullWeatherDTO getFullWeather(CityDTO city);
 }
