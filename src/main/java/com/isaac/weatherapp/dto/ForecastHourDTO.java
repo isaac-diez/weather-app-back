9570045a19420937bff38ebdd0381b93a33bf7af
isaac diez <isaac.diez@proton.me>
2026-03-23 21:10:07 +0100

    refactor: removed camelcase from fields to avoid jackson/lombok fails

--- src/main/java/com/isaac/weatherapp/dto/ForecastHourDTO.java
+++ src/main/java/com/isaac/weatherapp/dto/ForecastHourDTO.java
@@ -1,5 +1,6 @@
 package com.isaac.weatherapp.dto;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 
 @Data
@@ -11,11 +12,11 @@ public class ForecastHourDTO {
     private Double rain;
     private Double precipitation_probability;
     private Double cloudCover;
-    private Double uvIndex;
-    private Double uvIndexClearSky;
-    private Double sunshineDuration;
-    private Double shortwaveRadiation;
-    private Double directRadiation;
-    private Double diffuseRadiation;
+    private Double uv_index;
+    private Double uv_index_clear_sky;
+    private Double sunshine_duration;
+    private Double shortwave_radiation;
+    private Double direct_radiation;
+    private Double diffuse_radiation;
 
 }
