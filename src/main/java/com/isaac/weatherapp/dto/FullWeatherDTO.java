46466c48732132db0fe2d5d0355dd3b0d8e8bb15
isaac diez <isaac.diez@proton.me>
2026-04-25 11:38:40 +0200

    feat: update FullWeatherDTO to include AirQualityDTO and modify solarSummary field

--- src/main/java/com/isaac/weatherapp/dto/FullWeatherDTO.java
+++ src/main/java/com/isaac/weatherapp/dto/FullWeatherDTO.java
@@ -2,11 +2,24 @@ package com.isaac.weatherapp.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Data
 @AllArgsConstructor
 public class FullWeatherDTO {
     private CurrentWeatherDTO current;
     private ForecastDTO forecast;
-    private SolarSummaryDTO solarSummary;
+    private SolarSummaryDTO solar;
+    private AirQualityDTO airQuality;
+
+    public FullWeatherDTO(CurrentWeatherDTO current, ForecastDTO forecast, SolarSummaryDTO solar) {
+        this.current = current;
+        this.forecast = forecast;
+        this.solar = solar;
+    }
+
+    public void setAirQuality(AirQualityDTO airQuality) {
+        this.airQuality = airQuality;
+    }
 }
